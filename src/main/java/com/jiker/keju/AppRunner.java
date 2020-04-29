83d9626f6c0bf1b8f85a48edbe27d3e11606ce0d
edward <optihack@163.com>
2020-04-29 18:52:57 +0800

    modify AppRunner

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -3,7 +3,6 @@ package com.jiker.keju;
 public class AppRunner {
 
     public static void main(String[] args) throws Exception {
-        args = new String[]{"testData.txt"};
         TaxiReceipt taxiReceipt = new TaxiReceipt();
         String testDataFile = args[0];
         taxiReceipt.run(testDataFile);
